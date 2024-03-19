a10577163b4c7946daed000cd3f7d65fc6d0a0f2
zainab7681051 <zainab76810510@gmail.com>
2024-03-19 22:14:12 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,5 @@
-#include <SDL.h>
 #include "screen.h"
-
 int main(int argc, char* argv[]) {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        return 1;
-    }
 
-    // Create a window
-    auto window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-
-    // Main loop
-    bool quit = false;
-    SDL_Event e;
-   
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-    }
-
-    // Cleanup
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+        return 0;
 }
--- screen.h
+++ screen.h
@@ -1,8 +1,43 @@
 #pragma once
-//13:33
+#include <SDL.h>
+#include <vector>
 
 class Screen
 {
+	SDL_Event e;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	std::vector<SDL_FPoint> points;
 public:
-	Screen(){}
+	Screen()
+	{
+		SDL_Init(SDL_INIT_VIDEO);
+		SDL_CreateWindowAndRenderer(640 * 2, 480 * 2, 0, &window, &renderer);
+		SDL_RenderSetScale(renderer, 2, 2);
+	}
+	void pixel(float x, float y) 
+	{
+		points.emplace_back(x,y);
+	}
+	void show() {
+		SDL_SetRenderDrawColor(renderer,0,0,0,255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		for(auto& point: points)
+		{
+			SDL_RenderDrawPointF(renderer, point.x,point.y);
+		} 
+		SDL_RenderPresent(renderer);
+	}
+	void input() 
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				SDL_Quit();
+				exit(0);
+			}
+		}
+	}
 };
